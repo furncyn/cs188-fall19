de1ca23f3ce7ea80f7d817b27e1c1c1de2aee206
Furn Techalertumpai <furncyn@ucla.edu>
2019-10-24 18:12:06 -0700

    Complete computeBow

--- Homework1/code/homework1.py
+++ Homework1/code/homework1.py
@@ -5,7 +5,7 @@ import argparse
 parser = argparse.ArgumentParser(description='CS188.2 - Fall 19 - Homework 1')
 parser.add_argument("--tiny", "-t", type=bool, default=False, help='run Tiny Images')
 parser.add_argument("--create-path", "-cp", type=bool, default=True, help='create the Results directory')
-parser.add_argument("--create-vocabs", "-cv", type=bool, default=False, help='create a new set of vocabularies, instead of loading from Results directory')
+parser.add_argument("--build-dict", "-bd", type=bool, default=False, help='create a new set of vocabularies, instead of loading from Results directory')
 args = parser.parse_args()
 
 # The argument is included as an idea for debugging, with a few examples in the main. Feel free to modify it or add arguments.
@@ -54,14 +54,19 @@ if __name__ == "__main__":
     # e.g vocab_idx[i] will tell you which algorithms/neighbors were used to compute vocabulary i
     # This isn't used in the rest of the code so you can feel free to ignore it
 
-    # If create_vocabs flag is not specified, read vocabularies from specified directory of Results/ by default.
+    # If build_dict flag is not specified, read vocabularies from specified directory of Results/ by default.
     # Otherwise, generate a new set of vocabularies from train images.
-    for feature in ['sift', 'surf', 'orb']:
-        for algo in ['kmeans', 'hierarchical']:
+    if args.build_dict:
+        print("Calling buildDict to build vocabularies...")
+
+    for feature in ['surf', 'orb']:
+        for algo in ['hierarchical']:
             for dict_size in [20, 50]:
-                if args.create_vocabs:
+                if args.build_dict:
                     vocabulary = buildDict(train_images, dict_size, feature, algo)
                     filename = 'voc_' + feature + '_' + algo + '_' + str(dict_size) + '.npy'
+                    print(filename)
+                    print(vocabulary)
                     np.save(SAVEPATH + filename, np.asarray(vocabulary))
                 else:
                     filename = f"{SAVEPATH}/voc_{feature}_{algo}_{dict_size}.npy"
@@ -75,31 +80,42 @@ if __name__ == "__main__":
     features = ['sift'] * 4 + ['surf'] * 4 + ['orb'] * 4 # Order in which features were used 
     # for vocabulary generation
 
+    start_time = time.time()
     for i, vocab in enumerate(vocabularies):
-        print(i)
+        print(f"Calling computeBow on vocab {i}")
         for image in train_images: # Compute the BOW representation of the training set
             rep = computeBow(image, vocab, features[i]) # Rep is a list of descriptors for a given image
-            if rep: # only append rep if it is not None
+            if rep is not None: # only append rep if it is not None
                 train_rep.append(rep)
         np.save(SAVEPATH + 'bow_train_' + str(i) + '.npy', np.asarray(train_rep)) # Save the representations for vocabulary i
         train_rep = [] # reset the list to save the following vocabulary
+
         for image in test_images: # Compute the BOW representation of the testing set
             rep = computeBow(image, vocab, features[i])
-            if rep: # only append rep if it is not None
+            if rep is not None: # only append rep if it is not None
                 test_rep.append(rep)
         np.save(SAVEPATH + 'bow_test_' + str(i) + '.npy', np.asarray(test_rep)) # Save the representations for vocabulary i
         test_rep = [] # reset the list to save the following vocabulary
-        
+    runtime = time.time() - start_time
+    print("computeBow runtime is", runtime/60)
     
     # Use BOW features to classify the images with a KNN classifier
     # A list to store the accuracies and one for runtimes
     knn_accuracies = []
     knn_runtimes = []
 
-    # Your code below, eg:
-    # for i, vocab in enumerate(vocabularies):
-    # ... 
+    for i, vocab in enumerate(vocabularies):
+        train_rep = np.load(f"Results/bow_train_{i}.npy")
+        test_rep = np.load(f"Results/bow_test_{i}.npy")
+        start_time = time.time()
+        predicted_labels = KNN_classifier(train_features=train_rep, train_labels=train_labels, test_features=test_rep, num_neighbors=9)
+        runtime = time.time() - start_time
+        accuracy = reportAccuracy(test_labels, predicted_labels)
+        knn_accuracies.append(accuracy)
+        knn_runtimes.append(runtime)
     
+    print("accuracies:", knn_accuracies)
+    print("runtimes:", knn_runtimes)
     np.save(SAVEPATH+'knn_accuracies.npy', np.asarray(knn_accuracies)) # Save the accuracies in the Results/ directory
     np.save(SAVEPATH+'knn_runtimes.npy', np.asarray(knn_runtimes)) # Save the runtimes in the Results/ directory
     
