d9905c8f3af7a6c5f7e1b00d34819f22b04f5aef
Furn Techalertumpai <furncyn@ucla.edu>
2019-11-11 12:10:14 -0800

    Complete Task 2.4.1 and 2.4.2

--- Homework2/code/hw2.py
+++ Homework2/code/hw2.py
@@ -1,6 +1,19 @@
+import argparse
 import cv2
+import numpy as np
 import sys
 
+from matplotlib import pyplot as plt
+from skimage import data
+from skimage.feature import match_template
+
+rabbit_template_path = "../pictures/rabbit.jpg"
+
+
+parser = argparse.ArgumentParser(description="Run code for homework2")
+parser.add_argument("video_path",nargs="?",default="video.mp4",
+                    help="a path to video file to convert into frames")
+args = parser.parse_args()
 
 # Read video file and convert the video into a sequence of frames
 def readvideo(path):
@@ -20,5 +33,26 @@ def readvideo(path):
     cap.release()
     cv2.destroyAllWindows()
 
+
+# Calculate the Normalized Cross Correlation of the template
+def detect_template(img_path, template_path):
+    img = cv2.imread(img_path, 0)
+    template = cv2.imread(template_path, 0)
+    result = match_template(img, template, pad_input=True)
+    ij = np.unravel_index(np.argmax(result), result.shape)
+    x, y = ij[::-1] # Top left coordinate of the matching pattern is returned
+
+    fig = plt.figure(figsize=(10, 5))
+    ax = plt.subplot(1, 1, 1,
+        ylabel="Pixel location in Y direction",
+        xlabel="Pixel location in X direction"
+    )
+
+    ax.imshow(result, cmap=plt.cm.gray)
+    ax.autoscale(False)
+    plt.show()
+
+
 if __name__ == "__main__":
-    readvideo(sys.argv[1])
+    readvideo(args.video_path)
+    detect_template("../pictures/frame_0.jpg", rabbit_template_path)
